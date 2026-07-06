49868cfc2b00013db5f7df85d7b0477503e4803a
Jithu Tagore <jithu@zdistancelab.com>
2026-07-06 15:24:28 +0530

    changed  port

--- tools/run_webui_v2.py
+++ tools/run_webui_v2.py
@@ -114,4 +114,4 @@ if __name__ == "__main__":
         theme=args.theme,
         whisper_model_dir=str(args.whisper_model_dir),
     )
-    app.launch()
+    app.launch(server_name="0.0.0.0", share=True)
